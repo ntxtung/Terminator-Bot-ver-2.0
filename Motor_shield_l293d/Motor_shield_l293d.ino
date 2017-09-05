c53ed055c3e8f5f41ed96dd5f27c9fb9ef8109a6
ntxtung <xuantung.1998@gmail.com>
2017-09-05 23:40:09 +0700

    Init

--- Motor_shield_l293d/Motor_shield_l293d.ino
+++ Motor_shield_l293d/Motor_shield_l293d.ino
@@ -1,20 +1,16 @@
 #include <AFMotor.h>
- 
+
 AF_DCMotor motor1(1, MOTOR12_64KHZ); // tạo động cơ #1, 64KHz pwm  
 AF_DCMotor motor2(2, MOTOR12_64KHZ);
 AF_DCMotor motor3(3, MOTOR34_64KHZ);
 AF_DCMotor motor4(4, MOTOR34_64KHZ);
  
-void setup() {  
-    
-       
-    
+void setup() {
     motor1.setSpeed(255);     // chọn tốc độ maximum 255`/255
     motor2.setSpeed(255);
     motor3.setSpeed(255);
     motor4.setSpeed(255);
- 
-}  
+ }  
  
 void loop() 
 {        
@@ -28,7 +24,6 @@ void loop()
     motor2.run(BACKWARD);
     motor3.run(BACKWARD);
     motor4.run(BACKWARD);
-    //motor.run(RELEASE);      //   dừng động cơ
-    
+    //motor.run(RELEASE);      //   dừng động cơ  
  
 }
